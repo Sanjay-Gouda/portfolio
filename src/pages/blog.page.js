97122a4ca51697105268a6d61957eeeac0404c9b
Sanjaykumar Gouda <sanjay.kumar@oropocket.com>
2023-08-26 23:56:00 +0530

    update

--- src/pages/blog.page.js
+++ src/pages/blog.page.js
@@ -10,19 +10,20 @@ const Blog = () => {
   return (
     <PageBase>
       <Container className="sm:w-7/12 w-11/12">
-      <img alt="About Nxt.tech" src="/assets/svgs/about.svg" className="w-11/12 mx-auto mb-20" />
-       { BlogArticle.map(({ title, article }, index) => (
-         <article className="my-8" key={index}>
-           <ArticleTitle title={title}/>
-           <Paragraph>
-             {article}
-           </Paragraph>
-         </article>
-       ))  }
+        <img
+          alt="About Nxt.tech"
+          src="/assets/svgs/about.svg"
+          className="w-11/12 mx-auto mb-24"
+        />
+        {BlogArticle.map(({ title, article }, index) => (
+          <article className="my-8" key={index}>
+            <ArticleTitle title={title} />
+            <Paragraph>{article}</Paragraph>
+          </article>
+        ))}
       </Container>
     </PageBase>
   );
 };
-
 
 export default Blog;
